9640872d09369a9346dc5525e1968dc098559d19
pagaforr-droid <pagaforr@gmail.com>
2026-04-17 10:47:25 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
+
   content: [
+
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // <-- Aquí está la corrección (se añadió 'src/')
+
+    "./**/*.{js,ts,jsx,tsx}",
+
   ],
+
   theme: {
+
     extend: {
+
       fontFamily: {
+
         sans: ['Inter', 'sans-serif'],
+
       },
+
       colors: {
+
         primary: '#0f172a', // slate-900
+
         accent: '#2563eb', // blue-600
+
       }
+
     }
+
   },
+
   plugins: [],
+
 }
