20844fa6223c5ea0730f17f8c68dc967ad6fce48
pagaforr-droid <pagaforr@gmail.com>
2026-04-17 10:43:11 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Aquí está la corrección (se añadió 'src/')
   ],
   theme: {
     extend: {
